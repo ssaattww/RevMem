51b58c03d6c7bc77a702c0df4c73d75e1f871c11
T.W <36392290+ssaattww@users.noreply.github.com>
2026-07-24 20:35:34 +0900

    fix(t203): export corrected revision mapper

--- src/core/git-diff/index.ts
+++ src/core/git-diff/index.ts
@@ -1,1 +1,10 @@
-export * from "./git-diff-interval-mapping";
+export {
+  parseZeroContextGitDiff,
+  type GitDiffFile,
+  type GitDiffHunk,
+  type GitDiffIntervalMappingInput,
+  type GitDiffIntervalMappingResult,
+  type GitDiffMappingOptions,
+  type ParsedGitDiff
+} from "./git-diff-interval-mapping";
+export { mapReviewedIntervalsAcrossDiff } from "./revision-interval-mapper";
