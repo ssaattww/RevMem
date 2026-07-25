48d215890967b7781a7a3cf1fa5dd1b7da0a4106
T.W <36392290+ssaattww@users.noreply.github.com>
2026-07-25 13:41:59 +0900

    feat(t204): validated transition APIを公開

--- src/core/git-diff/index.ts
+++ src/core/git-diff/index.ts
@@ -9,10 +9,10 @@ export {
 } from "./git-diff-interval-mapping";
 export { mapReviewedIntervalsAcrossDiff } from "./revision-interval-mapper";
 export {
-  applyGitFileStateTransitions,
   type GitFileStateTransitionInput,
   type GitFileStateTransitionResult,
   type GitFileTransitionUnresolved,
   type GitFileTransitionUnresolvedReason,
   type GitNewFileStateInput
 } from "./git-file-state-transition";
+export { applyGitFileStateTransitions } from "./validated-git-file-state-transition";
