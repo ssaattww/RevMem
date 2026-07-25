f2e69e0e0dc5822d5553ec6f063f8a5767c3ce6b
T.W <36392290+ssaattww@users.noreply.github.com>
2026-07-25 09:20:35 +0900

    feat(t204): export file state transition API

--- src/core/git-diff/index.ts
+++ src/core/git-diff/index.ts
@@ -8,3 +8,10 @@ export {
   type ParsedGitDiff
 } from "./git-diff-interval-mapping";
 export { mapReviewedIntervalsAcrossDiff } from "./revision-interval-mapper";
+export {
+  applyGitFileStateTransitions,
+  type GitFileStateTransitionInput,
+  type GitFileStateTransitionResult,
+  type GitFileTransitionUnresolved,
+  type GitFileTransitionUnresolvedReason
+} from "./git-file-state-transition";
