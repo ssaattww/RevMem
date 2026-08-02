e3339d1dea2bf1311bce84078d3121e96c7c29b7
T.W <36392290+ssaattww@users.noreply.github.com>
2026-08-02 21:04:50 +0900

    feat(t504): export background Global recalculation

---
+++ src/application/global-understanding/index.ts
@@ -0,0 +1,15 @@
+/** Background Global-understanding recalculation application API. */
+export {
+  GlobalUnderstandingBackgroundRecalculator,
+  InMemoryGlobalUnderstandingProgressCache
+} from "./global-understanding-background-recalculator";
+
+export type {
+  GlobalUnderstandingBackgroundRecalculatorDependencies,
+  GlobalUnderstandingFileSource,
+  GlobalUnderstandingProgressCache,
+  GlobalUnderstandingRecalculationInput,
+  GlobalUnderstandingRecalculationProgress,
+  IncludedGlobalUnderstandingFile,
+  LoadedGlobalUnderstandingFile
+} from "./global-understanding-background-recalculator";
