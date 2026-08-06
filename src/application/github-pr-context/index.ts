acf219381c259018b9bfca8b8fbcc635556f40ef
T.W <36392290+ssaattww@users.noreply.github.com>
2026-08-07 07:52:56 +0900

    fix(t404): export immutable PR revision mapper

--- src/application/github-pr-context/index.ts
+++ src/application/github-pr-context/index.ts
@@ -1,3 +1,4 @@
 export * from "./contracts";
 export * from "./github-pull-request-context-resolver";
 export * from "./github-pull-request-context-layer-store";
+export * from "./immutable-pull-request-revision-mapper";
