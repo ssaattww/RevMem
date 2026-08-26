d252f5eec40128850c4dc3a29a8dd31b5d3d7f49
T.W <36392290+ssaattww@users.noreply.github.com>
2026-08-26 13:14:56 +0900

    Issue #90: 詳細operation診断を公開

--- src/application/operation-feedback/index.ts
+++ src/application/operation-feedback/index.ts
@@ -1,2 +1,9 @@
 export * from "./operation-feedback";
 export * from "./startup-feedback-composition";
+export {
+  OperationFeedback,
+  formatOperationLogEntry,
+  type OperationActivity,
+  type OperationDiagnosticDetail,
+  type OperationLogEntry,
+} from "./issue-90-detailed-operation-feedback";
